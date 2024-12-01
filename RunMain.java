9bd8e388bc3380ce7d252c07666cd2061ecd87a2
nhyl-beta <nhylcyecye@gmail.com>
2024-12-01 20:46:20 +0800

    Changes to main class
    - main class renamed
    - added printline

---
+++ RunMain.java
@@ -0,0 +1,9 @@
+public class RunMain{
+    public static void main(String[] args){
+        System.out.println("dfghjkl;");
+        System.out.println("dfghjkl;");
+        System.out.println("dfghjkl;");
+        System.out.println("dfghjkl;");
+    }
+    
+}
--- main.java
+++
@@ -1,6 +0,0 @@
-public class main-dev {
-    public static void main(String[] args){
-        System.out.println("dfghjkl;");
-    }
-    
-}
