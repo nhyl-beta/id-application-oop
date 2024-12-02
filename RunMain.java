a71ba07c336632cfc9e73b3be986862dd0d67bac
giah-yoldi <giah.zhekinah@gmail.com>
2024-12-02 17:31:43 +0800

    Added a new branch and added new features
    + new features
    + new method for clear

--- RunMain.java
+++ RunMain.java
@@ -1,54 +1,52 @@
 import java.util.Scanner;
 
-public class RunMain{
+public class RunMain {
     public static final Scanner sc = new Scanner(System.in);
 
-    public static void landingPage(String message){
+    public static void landingPage(String message) {
         int stringLength = message.length();
 
         for (int i = 0; i < stringLength; i++) {
             System.out.print("-");
         }
-
-        System.out.println(" ");
-        System.out.println(" ");
-
+        System.out.println();
         System.out.println(message);
-
-        System.out.println(" ");
-        
         for (int i = 0; i < stringLength; i++) {
             System.out.print("-");
         }
-        
-        System.out.println(" ");
-        System.out.println("Press enter to continue.");
-        String option = sc.nextLine();
+        System.out.println("\nPress Enter to continue.");
+        System.out.println("Press 1 to exit.");
 
-        while (option!=null) {
+        while (true) {
+            String option = sc.nextLine();
             if (option.isEmpty()) {
                 clearScreen();
-                System.out.println("HALOO EBERYWAN");
-            }
-
-            if (sc.hasNextLine()) {
-                option = sc.nextLine();
-            }else{
-                option = null;
+                LoginPage lg = new LoginPage();
+                lg.display();
+                break;
+            } else if (option.equals("1")) {
+                System.out.println("Exiting program. Goodbye!");
+                System.exit(0);
+            } else {
+                System.out.println("Invalid input. Please try again.");
             }
         }
     }
 
-    public static void clearScreen(){
+    public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
-    
-    public static void main(String[] args){
-        clearScreen();
-        String message = "     Welkam to the Philippines     ";
-        landingPage(message);
 
+    public static void main(String[] args) {
+        clearScreen();
+        String message = "     Welcome to the Philippines     ";
+        landingPage(message);
     }
-    
+}
+
+class LoginPage {
+    public void display() {
+        System.out.println("Login/Register Window");
+    }
 }
