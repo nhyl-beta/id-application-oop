e5fa66cc2c20b0ac187b4177b5c17a7b81608fc1
nhyl-beta <nhylcyecye@gmail.com>
2024-12-04 23:24:13 +0800

    Added test version
    Co-Authored-By: k-panguito <190271838+k-panguito@users.noreply.github.com>

---
+++ Dashboard.java
@@ -0,0 +1,14 @@
+public class Dashboard {
+    public void run(){
+        display();
+    }
+
+    public void display(){
+        testApplication();
+    }
+
+    //application form ibahin mo yung mga method name
+    public void testApplication(){
+        System.out.println("dito ka mag lalagay ng form mo");
+    }
+}
