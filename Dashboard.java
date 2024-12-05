15730892fc08073f3da8ead0f3d356d8406fa2ca
nhyl-beta <nhylcyecye@gmail.com>
2024-12-05 23:58:42 +0800

    Updated Dashboard.java

--- Dashboard.java
+++ Dashboard.java
@@ -3,7 +3,6 @@ import java.util.HashMap;
 public class Dashboard {
     private final HashMap<String, User> userDatabase;
 
-    // Constructor to initialize with userDatabase
     public Dashboard(HashMap<String, User> userDatabase) {
         this.userDatabase = userDatabase;
     }
@@ -15,7 +14,8 @@ public class Dashboard {
         while (true) {
             System.out.println("1. View Profile");
             System.out.println("2. Logout");
-            System.out.println("3. Exit");
+            System.out.println("3. Test Form");
+            System.out.println("4. Exit");
             System.out.print("Your choice: ");
             String choice = Main.sc.nextLine().trim();
 
@@ -27,6 +27,10 @@ public class Dashboard {
                     System.out.println("Logging out...");
                     return; // Exit to login screen
                 case "3":
+                    System.out.println("Test Form");
+                    testForm(user);
+                    break;
+                case "4":
                     System.out.println("Exiting program. Goodbye!");
                     Utils.saveUsers(userDatabase); // Save changes
                     System.exit(0);
@@ -40,13 +44,18 @@ public class Dashboard {
     public void displayProfile(User user) {
         Utils.clearScreen();
         Utils.displayFramedMessage("      Profile      ");
-        System.out.println("Username: " + user.getUsername());
+        System.out.println("\nUsername: " + user.getUsername());
         System.out.println("Name: " + user.getFirstName() + " " + user.getLastName());
         System.out.println("Age: " + user.getAge());
         System.out.println("Phone: " + user.getPhoneNumber());
+        System.out.println();
     }
 
-    public void testForm(){
-        
+    public void testForm(User user){
+        Utils.clearScreen();
+        Utils.displayFramedMessage("      Test Form      ");
+        System.out.println(user.getLastName());
+        System.out.println(user.getFirstName());
+        System.out.println(user.getMiddleName());
     }
 }
