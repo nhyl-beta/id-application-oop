e9e5794ca2e0658cae8c8020f8ebc20f9a1db8c8
nhyl-beta <nhylcyecye@gmail.com>
2024-12-06 22:42:53 +0800

    Updated the project
    Co-Authored-By: k-panguito <190271838+k-panguito@users.noreply.github.com>

--- User.java
+++ User.java
@@ -1,5 +1,10 @@
 public class User {
-    private final String username, lastName, firstName, middleName, phoneNumber, password;
+    private final String username;
+    private final String lastName;
+    private final String firstName;
+    private final String middleName;
+    private final String phoneNumber;
+    private final String password;
     private final int age;
 
     public User(String username, String lastName, String firstName, String middleName, int age, String phoneNumber, String password) {
@@ -38,5 +43,40 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    /**
+     * Creates a new User instance with an updated last name.
+     */
+    public User withLastName(String newLastName) {
+        return new User(username, newLastName, firstName, middleName, age, phoneNumber, password);
+    }
+
+    /**
+     * Creates a new User instance with an updated first name.
+     */
+    public User withFirstName(String newFirstName) {
+        return new User(username, lastName, newFirstName, middleName, age, phoneNumber, password);
+    }
+
+    /**
+     * Creates a new User instance with an updated middle name.
+     */
+    public User withMiddleName(String newMiddleName) {
+        return new User(username, lastName, firstName, newMiddleName, age, phoneNumber, password);
+    }
+
+    /**
+     * Creates a new User instance with an updated age.
+     */
+    public User withAge(int newAge) {
+        return new User(username, lastName, firstName, middleName, newAge, phoneNumber, password);
+    }
+
+    /**
+     * Creates a new User instance with an updated phone number.
+     */
+    public User withPhoneNumber(String newPhoneNumber) {
+        return new User(username, lastName, firstName, middleName, age, newPhoneNumber, password);
     }
 }
