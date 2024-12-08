47d9f2a53c5197d5d17bc16cb6a8a47042a779cf
nhyl-beta <nhylcyecye@gmail.com>
2024-12-08 21:21:14 +0800

    Fixed Utils.java

--- Utils.java
+++ Utils.java
@@ -46,9 +46,27 @@ public class Utils {
     }
 
     public static void displayFramedMessage(String message) {
-        System.out.println("\n" + "=".repeat(40));
-        System.out.println(message);
-        System.out.println("=".repeat(40) + "\n");
+        int totalWidth = 70; // Set the total width of the frame
+        int messageLength = message.length();
+        
+        if (messageLength > totalWidth) {
+            System.out.println("Message is too long to fit in the frame.");
+            return;
+        }
+    
+        int padding = (totalWidth - messageLength) / 2;
+        String paddedMessage = " ".repeat(padding) + message + " ".repeat(padding);
+    
+        // Add extra space if totalWidth is odd and messageLength is even (or vice versa)
+        if ((totalWidth - messageLength) % 2 != 0) {
+            paddedMessage += " ";
+        }
+    
+        System.out.println("=".repeat(totalWidth));
+        System.out.println();
+        System.out.println(paddedMessage);
+        System.out.println();
+        System.out.println("=".repeat(totalWidth));
     }
 
     public static String getValidDate(String prompt) {
